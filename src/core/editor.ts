65089588f2fc0dc8a2530d03cf350c7951bbb5d2
oklookat <onemandigitalk@gmail.com>
2022-01-05 23:37:59 +0300

    fix input linebreaks

--- src/core/editor.ts
+++ src/core/editor.ts
@@ -13,7 +13,7 @@ export default class EditorLoader {
         this.resizer = new TextareaResizer(this.element)
         this.config = config
         if (this.config.input) {
-            this.element.innerText = this.config.input
+            this.element.value = this.config.input
         }
         if (this.config.placeholder) {
             this.element.placeholder = this.config.placeholder
