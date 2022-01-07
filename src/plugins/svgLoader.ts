b1b2bb53bf415e6d472be4bd181332f09afe2c64
oklookat <onemandigitalk@gmail.com>
2022-01-07 23:43:58 +0300

    remove svgloader dupl

--- src/plugins/svgLoader.ts
+++ src/plugins/svgLoader.ts
@@ -47,27 +47,3 @@ export default class SVGLoader implements Plugin {
   }
 
 }
-
-
-export const svgLoader: (options?: {
-  classPrefix?: string
-  idPrefix?: string
-  removeSVGTagAttrs?: boolean
-  warnTags?: boolean
-  removeTags?: boolean
-  warnTagAttrs?: boolean
-  removingTagAttrs?: boolean
-}) => Plugin = (options?: {}) => {
-  return {
-    name: "vite-svg-patch-plugin",
-    transform: function (code, id) {
-      if (
-        id.endsWith(".svg")
-      ) {
-        const extractedSvg = fs.readFileSync(id, "utf8")
-        return `export default '${getExtractedSVG(extractedSvg, options)}'`
-      }
-      return code
-    }
-  }
-}
